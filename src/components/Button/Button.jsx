48bff2b2ce93f06b60204d55c5b4c9afe6356eff
dilipksahu <sahud048@gmail.com>
2024-05-20 15:54:56 +0530

    form nav and contact header completed

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import { MdMessage } from "react-icons/md";
+import styles from './Button.module.css';
+
+const Button = ({text, icon, isOutline}) => {
+  return (
+    <button className={isOutline ? styles.outline_btn : styles.primary_btn}>
+        {icon} {text}
+    </button>
+  )
+}
+
+export default Button
